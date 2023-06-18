02eaa5f8772e18cadf7e7c4655d9869c25a39b30
Bondarenko Vitalii <xydozhnikbomj@gmail.com>
2023-06-18 18:34:30 +0300

    init commit

---
+++ main.js
@@ -0,0 +1,42 @@
+class Hamburger {
+    constructor(size, contein) {
+      this.size = size;
+      this.contein = contein;
+      this.addOns = [];
+    }
+  
+    addAddOn(addOn) {
+      this.addOns.push(addOn);
+    }
+  
+    getPrice() {
+      const totalPrice = this.addOns.reduce((acc, addOn) => acc + addOn.price, this.size.price + this.stuffing.price);
+      return totalPrice;
+    }
+  
+    getCalories() {
+      const totalCalories = this.addOns.reduce((acc, addOn) => acc + addOn.calories, this.size.calories + this.stuffing.calories);
+      return totalCalories;
+    }
+  }
+  
+  const SMALL = { price: 50, calories: 20 };
+  const LARGE = { price: 100, calories: 40 };
+  
+  const CHEESE = { price: 10, calories: 20 };
+  const SALAD = { price: 20, calories: 5 };
+  const POTATOES = { price: 15, calories: 10 };
+  
+  const SEASONING = { price: 15, calories: 0 };
+  const MAYONNAISE = { price: 20, calories: 5 };
+  
+  const hamburger = new Hamburger(SMALL, CHEESE);
+  hamburger.addAddOn(SEASONING);
+  hamburger.addAddOn(MAYONNAISE);
+  
+  const price = hamburger.getPrice();
+  const calories = hamburger.getCalories();
+  
+  console.log(`price: ${price} `);
+  console.log(` calories: ${calories} `);
+  
